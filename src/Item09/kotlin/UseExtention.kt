05a91771ff7f8002f7077ea28d507c2c79d4d3b5
Ibrahim <sun_moon545@yahoo.com>
2018-11-30 16:46:37 +0200

    Item#10 Samples [Kotlin]

--- src/Item09/kotlin/UseExtention.kt
+++ src/Item09/kotlin/UseExtention.kt
@@ -5,12 +5,51 @@ package Item09.kotlin
 //Instead, Kotlin introduces the extension function use which will call close() on a Closeable
 // when the block provided completes or throws an exception.
 
-
 /*
 
-   inputStream.use {
-      // do something with the stream
-      it.read()
+inputStream.use {
+    outputStream.use {
+        // do something with the streams
+        outputStream.write(inputStream.read())
     }
+}
 
 */
+
+
+//If in your app you are constantly dealing with multiple resources then this can still look pretty messy,
+// but with Kotlin’s extension methods we can help improve this situation:
+
+/*
+private inline fun <T : Closeable?> Array<T>.use(block: ()->Unit) {
+    var exception: Throwable? = null
+    try {
+        return block()
+    } catch (e: Throwable) {
+        exception = e
+        throw e
+    } finally {
+        when {
+            exception == null -> forEach { it?.close() }
+            else -> forEach {
+                try {
+                    it?.close()
+                } catch (closeException: Throwable) {
+                    exception.addSuppressed(closeException)
+                }
+            }
+        }
+    }
+}
+
+*/
+
+//Allowing you to turn the original example into:
+
+/*
+arrayOf(inputStream, outputStream).use {
+    // do something with the streams
+    outputStream.write(inputStream.read())
+}
+*/
+
---
+++ src/Item10/kotlin/DataClass.kt
@@ -0,0 +1,9 @@
+package Item10.kotlin
+
+
+/*
+   The data class is one of Kotlin’s most touted features as they already implement equals for you.
+ */
+data class DataClass(val firstVal: String, val nVal: String) {
+
+}
